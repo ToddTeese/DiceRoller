b457e3df7ad9578b5a7a8311faab2b2ed8790997
Todd <teese.todd@gmail.com>
2021-04-17 22:24:26 +0100

    minor cleanup into functions

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+// ELEMENTS
 const buttonElement = document.querySelector('#rollButton');
 const resultsElement = document.querySelector('#diceResults');
+const diceSelector = document.querySelector('#diceSelector');
+// ELEMENTS
+
+// DATA
 let dice = {4:0, 6:0, 8:0, 10:0, 12:0, 20: 0, 100: 0}; // keys : diceSizes, values: numberOfDice
 let diceResults = {}
+// DATA
 
+
+// DICE FUNCTIONS
 function rollDie(size) {
     return Math.floor((Math.random() * size) + 1);
 }
@@ -17,100 +25,12 @@ function rollDice() {
         for(let j = 0; j < currentNumber; j++) {
             const value = rollDie(currentSize);
             diceResults[currentSize].push(value);
-            console.log(diceResults);
         }
     }
     displayDiceResults();
 }
 
-function displayDiceResults() {
-    const diceSizes = Object.keys(diceResults);
-    resultsElement.innerHTML = '';
-    const tableEle = document.createElement('table');
-    const tableHeader = document.createElement('thead');
-    const tableHeaderRow = document.createElement('tr');
-    const sizeTitle = document.createElement('th');
-    sizeTitle.innerHTML = 'Dice Size';
-    sizeTitle.scope = 'col';
-    tableHeaderRow.appendChild(sizeTitle);
-    const valueTitle = document.createElement('th');
-    valueTitle.innerHTML = 'Roll Values';
-    valueTitle.scope = 'col';
-    tableHeaderRow.appendChild(valueTitle);
-    tableHeader.appendChild(tableHeaderRow);
-    tableEle.appendChild(tableHeader);
-
-    const tableBody = document.createElement('tbody');
-    for(let i = 0; i < diceSizes.length; i++) {
-        const currentSize = diceSizes[i];
-        const currentValues = diceResults[currentSize];
-        // create row
-        const newRow = document.createElement('tr');
-        // create head
-        const newHead = document.createElement('th');
-        newHead.innerText = currentSize;
-        newRow.appendChild(newHead);
-        // create value
-        const newValue = document.createElement('td');
-        newValue.innerText = currentValues.join(', ');
-        newRow.appendChild(newValue);
-        tableBody.appendChild(newRow);
-    }
-    tableEle.appendChild(tableBody);
-
-    resultsElement.appendChild(tableEle);
-}
-
-buttonElement.onclick = () => {rollDice()}
-
-const diceSelector = document.querySelector('#diceSelector');
-
-// we need to build a methodology
-function displayDiceSelector() {
-    // use dice object
-    const diceSizes = Object.keys(dice);
-    diceSelector.innerHTML = ''; // kill values
-
-
-    for(let i = 0; i < diceSizes.length; i++) {
-        const currentSize = diceSizes[i];
-        const currentValue = dice[currentSize];
-        const newSection = document.createElement('div');
-        newSection.classList = 'diceSection';
-        newSection.style = 'display:flex; flex-direction:column';
-        // d6
-        const titleSpan = document.createElement('span');
-        titleSpan.innerText = 'd' + currentSize;
-        newSection.appendChild(titleSpan);
-        // d6
-
-        // + button
-        const plusButton = document.createElement('button');
-        plusButton.innerText = '+';
-        plusButton.onclick = () => {addDice(currentSize)}
-        newSection.appendChild(plusButton);
-        // + button
-
-        // Number
-        const valueSpan = document.createElement('span');
-        valueSpan.innerText = currentValue;
-        newSection.appendChild(valueSpan);
-        // Number
-
-        // - button
-        const minusButton = document.createElement('button');
-        minusButton.innerText = '-';
-        minusButton.onclick = () => {removeDice(currentSize)}
-        newSection.appendChild(minusButton);
-        // - button
-
-        diceSelector.appendChild(newSection);
-
-    }
-}
-
 function addDice(size) {
-    console.log(size);
     dice[size]++;
     displayDiceSelector();
 }
@@ -122,5 +42,106 @@ function removeDice(size) {
     }
     displayDiceSelector();
 }
+// DICE FUNCTIONS
+
+// DATA MANIPULATION
+function getTotal(values) {
+    let total = 0;
+    for(let i = 0; i < values.length; i++) {
+        total += values[i];
+    }
+    return total;
+}
+// DATA MANIPULATION
+
+// VIEW ELEMENT
+function newElement(type, innerHTML, options) {
+    const newEle = document.createElement(type);
+    newEle.innerText = innerHTML;
+    
+    if(options != undefined) {
+        const optionKeys = Object.keys(options);
+        for(let i = 0; i < optionKeys.length; i++) {
+            const currentOption = optionKeys[i];
+            newEle[currentOption] = options[currentOption];
+        }
+    }
+    return newEle;
+}
+// VIEW ELEMENT
+
+// DISPLAY
+function displayDiceResults() {
+    const diceSizes = Object.keys(diceResults);
+    resultsElement.innerHTML = '';
+    const tableEle = document.createElement('table');
+    const tableHeader = document.createElement('thead');
+    const tableHeaderRow = document.createElement('tr');
+
+    // add dice size header
+    tableHeaderRow.appendChild(newElement('th', 'Dice Size', {scope:'col'}));
+
+    // add roll values
+    tableHeaderRow.appendChild(newElement('th', 'Roll Values', {scope:'col'}));
+
+    // add roll totals
+    tableHeaderRow.appendChild(newElement('th', 'Roll Totals', {scope:'col'}));
+
+    tableHeader.appendChild(tableHeaderRow);
+    tableEle.appendChild(tableHeader);
+
+    const tableBody = document.createElement('tbody');
+    for(let i = 0; i < diceSizes.length; i++) {
+        const currentSize = diceSizes[i];
+        const currentValues = diceResults[currentSize];
+        // create row
+        const newRow = document.createElement('tr');
+        // create head
+        newRow.appendChild(newElement('th', currentSize));
+        // create value
+        newRow.appendChild(newElement('td', currentValues.join(', ')));
+        // create total
+        newRow.appendChild(newElement('td', getTotal(currentValues)));
+
+        tableBody.appendChild(newRow);
+    }
+    tableEle.appendChild(tableBody);
+
+    resultsElement.appendChild(tableEle);
+}
+
+function displayDiceSelector() {
+    // use dice object
+    const diceSizes = Object.keys(dice);
+    diceSelector.innerHTML = ''; // kill values
+
+    for(let i = 0; i < diceSizes.length; i++) {
+        const currentSize = diceSizes[i];
+        const currentValue = dice[currentSize];
+        
+        // const newSection = document.createElement('div');
+        const newSection = newElement('div', '', {classList: 'diceSection', style: 'display:flex; flex-direction:column'});
+
+        // d6
+        newSection.appendChild(newElement('span', 'd' + currentSize));
+
+        // + button
+        newSection.appendChild(newElement('button', '+', {onclick: () => {addDice(currentSize)}}));
+
+        // Number
+        newSection.appendChild(newElement('span', currentValue));
+
+        // - button
+        newSection.appendChild(newElement('button', '-', {onclick: () => {removeDice(currentSize)}}));
+
+        diceSelector.appendChild(newSection);
+
+    }
+}
+// DISPLAY
+
+buttonElement.onclick = () => {rollDice()}
+
+displayDiceResults();
 
 displayDiceSelector();
